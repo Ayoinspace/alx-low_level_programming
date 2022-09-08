5c51a7df03da783ea16e06180e809b7a0597b9b6
Ayoinspace <obatayo94@gmail.com>
2022-09-08 08:51:43 -0700

    Commited

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,19 @@
+/*
+ * File: 4-puts.c
+ * Auth: Brennan D Baraban <375@holbertonschool.com>
+ */
+
 #include <stdio.h>
 
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
 int main(void)
-{	
-	
-	puts("\"programming is like building a multilingual puzzle");	
+{
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
-
 }
