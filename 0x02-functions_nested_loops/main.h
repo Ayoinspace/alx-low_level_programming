e7ff0b17038bd3161b33b657d3f7d432a28aa962
Ayoinspace <obatayo94@gmail.com>
2022-09-14 05:24:13 -0700

    Commited

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,9 @@
-#ifndefine MAIN_H
-define MAIN_H
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio>
-int_putchar(char c);
+
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -12,8 +13,9 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-int add (int, int);
+int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
+
 
 #endif /* MAIN_H */
