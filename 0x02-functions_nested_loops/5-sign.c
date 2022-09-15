012837ce340289a678dc8899c16cf46d4abf2666
Ayoinspace <obatayo94@gmail.com>
2022-09-14 18:05:59 -0700

    Message committed

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ *print_sign - prints the sign of a number
+ *
+ *@n:input
+ *
+ *Description:prints sign
+ *
+ *Return: 1 - positive, 0 - zero, -1 - negative
+ *
+ */
+int print_sign(int n)
+{
+	int x;
+
+	if (n > 0)
+	{
+		x = 1;
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		x = 0;
+		_putchar('0');
+	}
+	else if (n < 0)
+	{
+		x = -1;
+		_putchar('-');
+	}
+	return (x);
