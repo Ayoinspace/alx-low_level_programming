9eb7cb50b4070a729d11fece0a7f181c34fafbf4
Ayoinspace <obatayo94@gmail.com>
2022-09-14 18:47:36 -0700

    Message committed

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *main - tests function with zero
+ *
+ *Description:testing with zero
+ *
+ *Return:returns zero(success)
+ *
+ */
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+	return (0);
+}
