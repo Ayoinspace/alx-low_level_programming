f4fe0f9cd8039104276b3c2752cf8bbd9f4bf303
Ayoinspace <obatayo94@gmail.com>
2022-09-14 18:50:09 -0700

    Message committed

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+int largest_number(int a, int b, int c)
+{
+	int test;
+	int largest;
+
+	if (a > b)
+	{
+		test = a;
+	}
+	else
+	{
+		test = b;
+	}
+	if (test > c)
+	{
+		largest = test;
+	}
+	else
+	{
+		largest = c;
+	}
+	return (largest);
+}
